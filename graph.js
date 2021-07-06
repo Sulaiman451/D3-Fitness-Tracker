5ca7fb308e0a3a2ab8985f948329e66e084f5de6
Sulaiman Rahman <rsulaiman451@gmail.com>
2021-07-06 19:40:57 +0100

    Domains & axes

--- graph.js
+++ graph.js
@@ -1,10 +1,10 @@
-const margin = { top: 40, right: 20, bottom: 50, left: 100 };
-const graphWidth = 560 - margin.left - margin.right;
-const graphHeight = 400 - margin.top - margin.bottom;
+const margin = { top: 20, right: 20, bottom: 50, left: 100 };
+const graphWidth = 560 - margin.right - margin.left;
+const graphHeight = 360 - margin.top - margin.bottom;
 
 const svg = d3
   .select(".canvas")
-  .appen("svg")
+  .append("svg")
   .attr("width", graphWidth + margin.left + margin.right)
   .attr("height", graphHeight + margin.top + margin.bottom);
 
@@ -16,42 +16,56 @@ const graph = svg
 
 // scales
 const x = d3.scaleTime().range([0, graphWidth]);
-const y = d3.scaleLinear().range([0, graphHeight]);
+const y = d3.scaleLinear().range([graphHeight, 0]);
 
 // axes groups
 const xAxisGroup = graph
   .append("g")
   .attr("class", "x-axis")
-  .attr("transform", `translate(0, ${graphHeight})`);
+  .attr("transform", "translate(0," + graphHeight + ")");
 
 const yAxisGroup = graph.append("g").attr("class", "y-axis");
 
+// update function
 const update = (data) => {
-  console.log(data);
+  // set scale domains
+  x.domain(d3.extent(data, (d) => new Date(d.date)));
+  y.domain([0, d3.max(data, (d) => d.distance)]);
+
+  // create axes
+  const xAxis = d3.axisBottom(x).ticks(4);
+
+  const yAxis = d3.axisLeft(y).ticks(4);
+
+  // call axes
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
 };
 
 // data and firestore
 var data = [];
 
-db.collection("activities").onSnapshot((res) => {
-  res.docChanges().forEach((change) => {
-    const doc = { ...change.doc.data(), id: change.doc.id };
+db.collection("activities")
+  .orderBy("date")
+  .onSnapshot((res) => {
+    res.docChanges().forEach((change) => {
+      const doc = { ...change.doc.data(), id: change.doc.id };
 
-    switch (change.type) {
-      case "added":
-        data.push(doc);
-        break;
-      case "modified":
-        const index = data.findIndex((item) => item.id === doc.id);
-        data[index] = doc;
-        break;
-      case "removed":
-        data = data.filter((item) => item.id !== doc.id);
-        break;
-      default:
-        break;
-    }
+      switch (change.type) {
+        case "added":
+          data.push(doc);
+          break;
+        case "modified":
+          const index = data.findIndex((item) => item.id == doc.id);
+          data[index] = doc;
+          break;
+        case "removed":
+          data = data.filter((item) => item.id !== doc.id);
+          break;
+        default:
+          break;
+      }
+    });
+
+    update(data);
   });
-
-  update(data);
-});
